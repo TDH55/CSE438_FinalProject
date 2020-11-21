9fb3579d2c5cc4db2d20696516f4d13934098394
Taylor Howard <tdhoward50@gmail.com>
2020-11-20 23:11:57 -0600

    Home view storyboard skeleton

--- CSE438_FinalProject/ViewControllers/HomeViewController/HomeViewController.swift
+++ CSE438_FinalProject/ViewControllers/HomeViewController/HomeViewController.swift
@@ -11,6 +11,7 @@ import Koloda
 
 class HomeViewController: UIViewController {
 
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
---
+++ CSE438_FinalProject/Views/CardView.swift
@@ -0,0 +1,9 @@
+//
+//  CardView.swift
+//  CSE438_FinalProject
+//
+//  Created by Taylor Howard on 11/20/20.
+//  Copyright © 2020 Taylor Howard. All rights reserved.
+//
+
+import Foundation
