9b787f4a3edf6aeb76bf7f43456dbb83b3f82d11
Taylor Howard <tdhoward50@gmail.com>
2020-12-11 00:39:53 -0600

    table cell setup/styling

--- CSE438_FinalProject/ViewControllers/SongListVC.swift
+++ CSE438_FinalProject/ViewControllers/SongListVC.swift
@@ -9,6 +9,15 @@
 import UIKit
 import CoreData
 
+class SongTableViewCell: UITableViewCell {
+    @IBOutlet weak var albumCoverImage: UIImageView!
+    @IBOutlet weak var songTitleLabel: UILabel!
+    @IBOutlet weak var songArtistLabel: UILabel!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var dislikeButton: UIButton!
+    
+}
+
 class SongListVC: UIViewController {
     
     @IBOutlet weak var songTableView: UITableView!
@@ -22,7 +31,8 @@ class SongListVC: UIViewController {
 
         // Do any additional setup after loading the view.
         //TODO: dispatch queue
-        songTableView.dataSource = self;
+        songTableView.dataSource = self
+        songTableView.delegate = self
         DispatchQueue.global(qos: .userInitiated).async {
             self.getSongList()
             DispatchQueue.main.async {
@@ -52,12 +62,33 @@ extension SongListVC: UITableViewDataSource{
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
-        cell.textLabel!.text = songList[indexPath.row].value(forKey: "name") as? String
+//        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! SongTableViewCell
+        cell.songTitleLabel!.text = songList[indexPath.row].value(forKey: "name") as? String
+        cell.songArtistLabel!.text = songList[indexPath.row].value(forKey: "artistName") as? String
         
+        if let isLiked = songList[indexPath.row].value(forKey: "liked") as? Bool {
+            if isLiked {
+                print("liked")
+                cell.likeButton.setImage(UIImage(systemName: "hand.thumbsup.fill"), for: .normal)
+            }else{
+                print("disliked")
+//                cell.dislikeButton.imageView?.image = UIImage(named: "hand.thumbsdown.fill")
+                cell.dislikeButton.setImage(UIImage(systemName: "hand.thumbsdown.fill"), for: .normal)
+            }
+        }
+    
         return cell
     }
     
+}
+
+extension SongListVC: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100.0
+    }
+    
+//    func tableView
 }
 
 //MARK: core data functions
