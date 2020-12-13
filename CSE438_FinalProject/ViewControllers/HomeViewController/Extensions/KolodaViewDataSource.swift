10cc5d62b160a354534ad4ada82376713963da4c
humzasiddiqui <hsiddiqui@wustl.edu>
2020-12-13 14:50:40 -0600

    Image for card almost done

--- CSE438_FinalProject/ViewControllers/HomeViewController/Extensions/KolodaViewDataSource.swift
+++ CSE438_FinalProject/ViewControllers/HomeViewController/Extensions/KolodaViewDataSource.swift
@@ -11,15 +11,26 @@ import Koloda
 
 
 extension HomeViewController: KolodaViewDataSource{
+    
+    //var imageCache:[UIImage] = []
+    
     func koloda(_ koloda: KolodaView, viewForCardAt index: Int) -> UIView {
         //TODO: create image cache? and give an image for the card here
+        if apiManager?.currentSong?.artworkURL != nil {
+            let url = (apiManager?.currentSong?.artworkURL)!
+        let urlOne = URL(string:  url)
+        let data = try? Data(contentsOf: urlOne!)
+        let image = UIImage(data: data!)
+            return UIImageView(image: image)
+        } else {
         return UIImageView(image: UIImage(systemName: "star"))
+        }
     }
     
     //TODO: create overlay for the card
-//    func koloda(_ koloda: KolodaView, viewForCardOverlayAt index: Int) -> OverlayView? {
-//        return Bundle.main.loadNibNamed("OverlayView", owner: self, options: nil)[0] as? OverlayView
-//    }
+    func koloda(_ koloda: KolodaView, viewForCardOverlayAt index: Int) -> OverlayView? {
+        return Bundle.main.loadNibNamed("OverlayView", owner: self, options: nil)?[0] as? OverlayView
+    }
     
     func kolodaNumberOfCards(_ koloda: KolodaView) -> Int {
         return songList.count
